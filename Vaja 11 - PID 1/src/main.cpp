6e3ff019fdf7cebca0d0e4ba20a51a68c76161a6
leonbl <leon.blazic@gmail.com>
2022-05-09 14:41:19 +0200

    Update main.cpp

--- Vaja 11 - PID 1/src/main.cpp
+++ Vaja 11 - PID 1/src/main.cpp
@@ -4,10 +4,13 @@ void sample_time(void);
 void calculate_PID(void);
 
 uint32_t last_t = 0;
-float error, last_error, error_sum;
-float Kp=50.0, Ki=0.0, Kd=0.0;
-float up = 0.0, ui = 0.0, ud = 0.0, u;
-float r, y;
+double error, last_error, error_sum;
+double Kp=0.05, Ki=0.01, Kd=0.02;
+double up = 0.0, ui = 0.0, ud = 0.0, u;
+double r, y;
+// PID calculation frequency
+uint32_t PIDf = 1000;
+double PIDT = 1/(double)PIDf;
 
 TIM_TypeDef *Instance1 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(6), PinMap_PWM);
 uint32_t channel1 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(6), PinMap_PWM));
@@ -15,16 +18,16 @@ HardwareTimer *MyTim1 = new HardwareTimer(Instance1);
 
 void setup() {
   MyTim1->setMode(channel1, TIMER_OUTPUT_COMPARE_PWM1, 6);
-  MyTim1->setOverflow(1, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
+  MyTim1->setOverflow(100, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
   MyTim1->resume();
 
   TIM_TypeDef *moj_tim = TIM3;
   HardwareTimer *casovnik = new HardwareTimer(moj_tim);
-  casovnik->setOverflow(100, HERTZ_FORMAT);
+  casovnik->setOverflow(PIDf, HERTZ_FORMAT);
   casovnik->attachInterrupt(sample_time);
   casovnik->resume();
   Serial.begin(115200);
-  r = 1000;
+  r = 600;
 }
 
 void loop() {
@@ -44,15 +47,25 @@ void calculate_PID(void){
   float t = millis()/1000.0;
   y = analogRead(A0);
   error = r - y;
-  up = error * Kp;
 
+  // P
+  up = error * Kp;
+  // I
+  error_sum += error;
+  if(error_sum > 100) error_sum=100;
+  else if (error_sum<0) error_sum=0;
+  ui = Ki * PIDT * error_sum;
+  // D
+  ud = Kd * PIDf * (error - last_error);
 
   u = up + ui + ud;
 
   if(u>100) u=100;
   else if (u<0) u=0;
-  u=500;
+
   MyTim1->setCaptureCompare(channel1, u, PERCENT_COMPARE_FORMAT);
+
+  last_error = error;
 
   Serial.print(t); 
   Serial.print("\t");
