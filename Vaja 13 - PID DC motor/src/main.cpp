87788effa82a47bf9ef6dea33f1d92abc9513cba
leonbl <leon.blazic@gmail.com>
2022-05-19 09:40:57 +0200

    ...

--- Vaja 13 - PID DC motor/src/main.cpp
+++ Vaja 13 - PID DC motor/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include "QuickPID.h"
 
 #define pinAA 4
 #define pinAB 5
@@ -10,13 +9,13 @@ void motor(int8_t hitrost);
 void encoder(void);
 void prekinitev_pid(void);
 
-float Setpoint, Input, Output, u;
-float Kp = 5, Ki = 0, Kd = 0;
+float error=0;
+float y, u;
+float r;
+float Kp=0.1;
 int8_t u_map;
 
 static int32_t counter = 0;
-
-QuickPID myPID(&Input, &Output, &Setpoint);
 
 TIM_TypeDef *Instance1 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(pinAA), PinMap_PWM);
 uint32_t channel1 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(pinAA), PinMap_PWM));
@@ -34,16 +33,16 @@ void setup() {
 
   TIM_TypeDef *moj_tim = TIM3;
   HardwareTimer *casovnik = new HardwareTimer(moj_tim);
-  casovnik->setOverflow(1000, HERTZ_FORMAT);
+  casovnik->setOverflow(1000, MICROSEC_FORMAT);
   casovnik->attachInterrupt(prekinitev_pid);
   casovnik->resume();
 
   MyTim1->setMode(channel1, TIMER_OUTPUT_COMPARE_PWM1, pinAA);
-  MyTim1->setOverflow(500, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
+  MyTim1->setOverflow(800, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
   MyTim1->resume();
 
   MyTim2->setMode(channel2, TIMER_OUTPUT_COMPARE_PWM1, pinAB);
-  MyTim2->setOverflow(500, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
+  MyTim2->setOverflow(800, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
   MyTim2->resume();
 
   MyTim1->setCaptureCompare(channel1, 0, PERCENT_COMPARE_FORMAT);
@@ -51,19 +50,15 @@ void setup() {
 
   attachInterrupt(digitalPinToInterrupt(encA), encoder, RISING);
   Serial.begin(9600);
-  Setpoint = 5000;
-  //apply PID gains
-  myPID.SetTunings(Kp, Ki, Kd);
-  //turn the PID on
-  myPID.SetMode(myPID.Control::automatic);
-  myPID.SetOutputLimits(-100, 100);
+  motor(27);
+  r=1000.0;
 }
 
 void loop() {
-  Serial.print(Output);
+  Serial.print(u_map);
   Serial.print(" ");
   Serial.println(counter);
-  delay(10);
+  delay(100);
 }
 
 void motor(int8_t hitrost){
@@ -91,10 +86,12 @@ void encoder(void){
 }
 
 void prekinitev_pid(void){
-    Input = counter;
-    myPID.Compute();
-    u = Output;
-    if(u>0) u_map = map(u, 0, 100, 27, 100 );
-    if(u<0) u_map = map(u, 0, -100, -50, -100 );
-    motor((int8_t)u_map);
+  y = (float)counter;
+  error = r - y;
+  u = Kp * error;
+  if(u>100) u=100;
+  if(u<-100) u=-100;
+  if(u>0) u_map = map(u, 0, 100, 30, 100 );
+  if(u<0) u_map = map(u, 0, -100, -50, -100 );
+  motor((int8_t)u_map);
 }
