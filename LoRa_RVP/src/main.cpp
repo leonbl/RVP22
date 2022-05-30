f5cfe5b39f5f28abc61132316112f0acd981c4c7
leonbl <leon.blazic@gmail.com>
2022-05-30 12:56:13 +0200

    ..

--- LoRa_RVP/src/main.cpp
+++ LoRa_RVP/src/main.cpp
@@ -5,7 +5,7 @@
  * Copyright (c) 2018 Terry Moore, MCCI
  * 
  * 
- * 
+ *   
  *
  * Permission is hereby granted, free of charge, to anyone
  * obtaining a copy of this document and accompanying files,
